993817796eaf875b5a595bdfc99d8a6337c5801c
PrinceYub <karkit@lsbu.ac.uk>
2024-11-26 22:01:53 +0000

    changes in nex.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
+import authRoutes from "./routes/AuthRoutes.js";
+import cookieParser from "cookie-parser";
+import { gigRoutes } from "./routes/GigRoutes.js";
+import { orderRoutes } from "./routes/OrderRoutes.js";
+import { messageRoutes } from "./routes/MessageRoutes.js";
+import { dashboardRoutes } from "./routes/DashboardRoutes.js";
 
 dotenv.config();
 
@@ -16,6 +22,14 @@ app.use(
   );
   
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/gigs", gigRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
