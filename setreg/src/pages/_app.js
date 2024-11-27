18f60c2434f4dc05209c549e3a4202bab99c9952
Mohamed Yusuf <hatemancity2@gmail.com>
2024-11-27 10:30:56 +0000

    Updated _app.js to include session provider and theme management

--- setreg/src/pages/_app.js
+++ setreg/src/pages/_app.js
@@ -1,1 +1,19 @@
+import '../styles/globals.css';  // Global styles
+import { useState } from 'react';  // Example hook import
+import { SessionProvider } from 'next-auth/react';  // If using NextAuth.js for authentication
+
+function MyApp({ Component, pageProps }) {
+  // You can manage global state or perform other setup here
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <SessionProvider session={pageProps.session}>
+      <div className={`theme-${theme}`}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
 
